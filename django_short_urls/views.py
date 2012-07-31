347d95d72c685207e787670dfe8fc5b61e553526
Olivier Le Floch <olivier@le-floch.fr>
2012-07-30 19:22:20 -0700

    Fix returning status code

--- django_short_urls/views.py
+++ django_short_urls/views.py
@@ -54,11 +54,11 @@ HTTP_BAD_REQUEST  = 400
 HTTP_UNAUTHORIZED = 401
 HTTP_CONFLICT     = 409
 
-def response(message=None, status_code=HTTP_OK, **kwargs):
+def response(message=None, status=HTTP_OK, **kwargs):
     kwargs.update({
-        "status_code": status_code,
-        "error": status_code != HTTP_OK,
+        "status_code": status,
+        "error": status != HTTP_OK,
         "message": message
     })
 
-    return HttpResponse(json.dumps(kwargs), status=status_code, mimetype="application/json")
+    return HttpResponse(json.dumps(kwargs), status=status, mimetype="application/json")
