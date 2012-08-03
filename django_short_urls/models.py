d91953361dd15c5e7b62789b8defe4c30832ac77
Olivier Le Floch <olivier@le-floch.fr>
2012-08-02 17:54:55 -0700

    MAke prefix a required field

--- django_short_urls/models.py
+++ django_short_urls/models.py
@@ -22,7 +22,7 @@ class Link(Document):
     short_path          = StringField(required=True)
     short_path_to_lower = StringField(required=True, unique=True)
     long_url            = StringField(required=True)
-    prefix              = StringField()
+    prefix              = StringField(required=True)
     creator             = StringField(required=True)
     created_at          = DateTimeField(required=True)
 
