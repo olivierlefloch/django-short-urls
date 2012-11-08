c8eeec16563d0c5a43af97fc1bf0eddb20c4f1bd
Olivier Le Floch <olivier@le-floch.fr>
2012-11-08 13:08:10 -0800

    Ensure that ban words are case insensitive (fixes WFU-2417)

--- django_short_urls/models.py
+++ django_short_urls/models.py
@@ -19,7 +19,7 @@ class ForbiddenKeyword(Exception):
 
     @classmethod
     def is_banned(cls, keyword):
-        return keyword in cls.ban_words
+        return keyword.lower() in cls.ban_words
 
     @classmethod
     def raise_if_banned(cls, keyword):
