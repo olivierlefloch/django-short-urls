43bffc7e9e432c8cc0cfeb813fd2c16ce2cbca77
Olivier Le Floch <olivier@le-floch.fr>
2012-11-08 14:34:45 -0800

    Support keyword being null

--- django_short_urls/models.py
+++ django_short_urls/models.py
@@ -19,7 +19,7 @@ class ForbiddenKeyword(Exception):
 
     @classmethod
     def is_banned(cls, keyword):
-        return keyword.lower() in cls.ban_words
+        return keyword is None or keyword.lower() in cls.ban_words
 
     @classmethod
     def raise_if_banned(cls, keyword):
