7f1a617c1f92acfb4bfb76b8e2217bdb61bc4573
Olivier Le Floch <olivier@le-floch.fr>
2012-12-04 16:54:22 -0800

    Setup a robots.txt for workfor.us (Fixes WFU-2676)

--- django_short_urls/urls.py
+++ django_short_urls/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^robots\.txt$', direct_to_template,
+        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+
     url(r'^api/v1/new$', 'django_short_urls.views.new', name='new'),
     url(r'^(.*)$', 'django_short_urls.views.main', name='main'),
 
