c8316a2a3a15983c6d68e37482b413c8a1950e79
Olivier Le Floch <olivier@le-floch.fr>
2012-12-04 16:54:04 -0800

    Remove useless lines in urls.py

--- django_short_urls/urls.py
+++ django_short_urls/urls.py
@@ -8,9 +8,6 @@ urlpatterns = patterns('',
     url(r'^api/v1/new$', 'django_short_urls.views.new', name='new'),
     url(r'^(.*)$', 'django_short_urls.views.main', name='main'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
