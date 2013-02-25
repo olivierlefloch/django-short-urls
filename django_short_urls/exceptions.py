338cd9b33810be2b2cef307610016f87b3bdc79a
Olivier Le Floch <olivier@le-floch.fr>
2013-02-25 11:52:08 -0800

    Move FIXME

--- django_short_urls/exceptions.py
+++ django_short_urls/exceptions.py
@@ -1,5 +1,5 @@
-# FIXME: Move to a dedicated ServiceUnavailable app
 from django.db.utils import DatabaseError
 
+# FIXME: Move to a dedicated ServiceUnavailable app
 class DatabaseWriteDenied(DatabaseError):
     pass
