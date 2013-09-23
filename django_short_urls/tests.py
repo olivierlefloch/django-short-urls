7acd95aa3a05e2200ea6e2864fcccb5a3b22c505
Olivier Le Floch <olivier@le-floch.fr>
2013-09-23 12:38:54 -0700

    Update unit tests - expect a dict

--- django_short_urls/tests.py
+++ django_short_urls/tests.py
@@ -27,7 +27,7 @@ class ValidateUrlTestCase(unittest.TestCase):
         self.assertEqual(validate_url('ftp://work4labs.com')[0], False)
         self.assertEqual(validate_url('http://app:bar@work4labs.com')[0], False)
 
-from suffix_catchall import get_hash_from, url_append_parameters, REDIRECT_PARAM_NAME
+from suffix_catchall import get_hash_from, url_append_parameters
 
 class ValidRedirectPathTestCase(unittest.TestCase):
     def test__get_hash_from(self):
@@ -45,22 +45,22 @@ class ValidRedirectPathTestCase(unittest.TestCase):
 
     def test__url_append_parameters(self):
         self.assertEqual(
-            url_append_parameters('http://workfor.us', None),
+            url_append_parameters('http://workfor.us', dict()),
             'http://workfor.us'
         )
         self.assertEqual(
-            url_append_parameters('http://workfor.us', 'toto'),
-            'http://workfor.us?%s=toto' % REDIRECT_PARAM_NAME
+            url_append_parameters('http://workfor.us', {'toto': 'tata'}),
+            'http://workfor.us?toto=tata'
         )
         self.assertEqual(
-            url_append_parameters('http://www.theuselessweb.com/', 'search'),
-            'http://www.theuselessweb.com/?%s=search' % REDIRECT_PARAM_NAME
-        )
-        self.assertRegexpMatches(
-            url_append_parameters('http://www.theuselessweb.com?a=1&b=2&z=5', 'search'),
-            r'^http://www.theuselessweb.com\?.*&%s=search&.*$' % REDIRECT_PARAM_NAME
+            url_append_parameters('http://www.theuselessweb.com/', {'foo': 'search'}),
+            'http://www.theuselessweb.com/?foo=search'
         )
         self.assertEqual(
-            url_append_parameters('http://www.theuselessweb.com?%s=4' % REDIRECT_PARAM_NAME, 'search'),
-            'http://www.theuselessweb.com?%s=search' % REDIRECT_PARAM_NAME
+            url_append_parameters('http://www.theuselessweb.com?a=1&b=2&z=5', {'foo': 'search'}),
+            'http://www.theuselessweb.com?a=1&b=2&z=5&foo=search'
+        )
+        self.assertEqual(
+            url_append_parameters('http://www.theuselessweb.com?foo=4', {'foo': 'search'}),
+            'http://www.theuselessweb.com?foo=search'
         )
