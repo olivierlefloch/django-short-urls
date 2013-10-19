0417c7e438c6e3cab9a1efe10341c568123d1cf4
Olivier Le Floch <olivier@le-floch.fr>
2013-10-18 21:10:13 -0700

    Start installing pywork4core

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
+
+'''Wrapper around django-admin.py, it will set the DJANGO_SETTING_MODULES environment variable.'''
+
+from __future__ import unicode_literals
+
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_short_urls.settings")
+    # we can not use setdefault since it will not work when calling from another python script
+    os.environ['DJANGO_SETTINGS_MODULE'] = "salesops.settings"
 
     from django.core.management import execute_from_command_line
 
