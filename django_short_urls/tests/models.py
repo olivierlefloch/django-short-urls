145c72c968388124313ee38135d81cc1489eaff7
Olivier Le Floch <olivier@le-floch.fr>
2013-10-19 10:13:26 -0700

    Separate files for separate test cases

---
+++ django_short_urls/tests/models.py
@@ -0,0 +1,17 @@
+from django.utils import unittest
+
+from django_short_urls.models import Link
+
+
+class ValidRandomShortPathsTestCase(unittest.TestCase):
+    def test(self):
+        self.assertEqual(Link.is_valid_random_short_path("ab2cd"), True)
+        self.assertEqual(Link.is_valid_random_short_path("ab2"), True)
+        self.assertEqual(Link.is_valid_random_short_path("a234r434g43gb32r"), True)
+        self.assertEqual(Link.is_valid_random_short_path("4a"), True)
+
+        self.assertEqual(Link.is_valid_random_short_path("abcd"), False)
+        self.assertEqual(Link.is_valid_random_short_path("ge"), False)
+        self.assertEqual(Link.is_valid_random_short_path("crap"), False)
+        self.assertEqual(Link.is_valid_random_short_path("crap42"), False)
+        self.assertEqual(Link.is_valid_random_short_path("abe4abe"), False)
---
+++ django_short_urls/tests/w4l_http.py
@@ -0,0 +1,36 @@
+from django.utils import unittest
+
+from django_short_urls.w4l_http import validate_url, url_append_parameters
+
+
+class W4lHttpTestCase(unittest.TestCase):
+    def test(self):
+        self.assertEqual(validate_url('http://workfor.us'), (True, None))
+        self.assertEqual(validate_url('http://app.work4labs.com/jobs?job_id=42'), (True, None))
+
+        self.assertEqual(validate_url('foobar')[0], False)
+        self.assertEqual(validate_url('jobs?job_id=42')[0], False)
+        self.assertEqual(validate_url('ftp://work4labs.com')[0], False)
+        self.assertEqual(validate_url('http://app:bar@work4labs.com')[0], False)
+
+    def test__url_append_parameters(self):
+        self.assertEqual(
+            url_append_parameters('http://workfor.us', dict(), dict()),
+            'http://workfor.us'
+        )
+        self.assertEqual(
+            url_append_parameters('http://workfor.us', {'toto': 'tata'}, dict()),
+            'http://workfor.us?toto=tata'
+        )
+        self.assertEqual(
+            url_append_parameters('http://www.theuselessweb.com/', {'foo': 'search'}, {'foo': 'bar'}),
+            'http://www.theuselessweb.com/?foo=search'
+        )
+        self.assertEqual(
+            url_append_parameters('http://www.theuselessweb.com?a=1&b=2&z=5', {'foo': 'search'}, {'b': '3'}),
+            'http://www.theuselessweb.com?a=1&b=2&z=5&foo=search'
+        )
+        self.assertEqual(
+            url_append_parameters('http://www.theuselessweb.com?foo=4', {'foo': 'search'}, {'c': '42'}),
+            'http://www.theuselessweb.com?c=42&foo=search'
+        )
