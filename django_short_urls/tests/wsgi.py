72623e41bf5277be02215cf6e2aac08e067fad32
Olivier Le Floch <olivier@le-floch.fr>
2013-10-19 15:08:37 -0700

    Test wsgi (sort of)

---
+++ django_short_urls/tests/wsgi.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.utils import unittest
+
+import django_short_urls.wsgi
+
+
+class WsgiTestCase(unittest.TestCase):
+    def test(self):
+        pass
