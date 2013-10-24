ac3f5c1843998284c41a02661cd793c902dd4303
Olivier Le Floch <olivier@le-floch.fr>
2013-10-23 23:00:05 -0700

    Filter on short_path instead (some might not have a prefix...) and also remove created_at

--- django_short_urls/migrate/20131021-OPS-1416-remove_link_prefix_post.js
+++ django_short_urls/migrate/20131021-OPS-1416-remove_link_prefix_post.js
@@ -3,8 +3,8 @@
 db.link.dropIndex({"prefix": 1, "long_url": 1})
 
 db.link.update(
-    {prefix: {$exists: true}},
-    {$unset: {prefix: 1, short_path: 1}},
+    {short_path: {$exists: true}},
+    {$unset: {prefix: 1, short_path: 1, created_at: 1}},
     /* upsert */ false,
     /* multi */ true
 );
