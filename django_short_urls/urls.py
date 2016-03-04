d3c2a2f485bd3bf314bb191a04608adf5af20df5
Olivier Le Floch <olivier@le-floch.fr>
2016-03-04 15:40:06 -0800

    We don't use the adming

--- django_short_urls/urls.py
+++ django_short_urls/urls.py
@@ -10,10 +10,6 @@ from django.views.generic.base import TemplateView
 
 # pylint: disable=C0103, E1120
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns(
     '',
     (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
@@ -24,7 +20,4 @@ urlpatterns = patterns(
 
     url(r'^api/v1/new$', 'django_short_urls.views.new', name='new'),
     url(r'^(.*)$', 'django_short_urls.views.main', name='main'),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
