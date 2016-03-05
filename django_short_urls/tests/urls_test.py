01cc7893328b82dd0c4d443faad4e6728162ae99
Olivier Le Floch <olivier@le-floch.fr>
2016-03-04 16:25:20 -0800

    Fix lint

--- django_short_urls/tests/urls_test.py
+++ django_short_urls/tests/urls_test.py
@@ -8,6 +8,8 @@ from http.status import HTTP_OK
 
 class UrlsTestCase(PyW4CTestCase):
     def test_robots(self):
+        # Sadly pylint doesn't detect members on WSGIRequest properly...
+        # pylint: disable=no-member
         response = self.client.get('/robots.txt')
 
         self.assertEqual(response.status_code, HTTP_OK)
