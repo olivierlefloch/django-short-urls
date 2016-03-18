d167ffcd9ddbf026ab47e94ace236a8cca139b87
Olivier Le Floch <olivier@le-floch.fr>
2016-03-18 14:12:10 -0700

    Fix deprecation warnings (BAT-5507)

--- django_short_urls/urls.py
+++ django_short_urls/urls.py
@@ -4,19 +4,19 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from django_short_urls.static_view import StaticView
+from django_short_urls import views
 
 
-urlpatterns = patterns(  # pylint: disable=invalid-name
-    '',
-    (r'^robots\.txt$', StaticView.as_view(template_name='robots.txt', content_type='text/plain')),
-    (r'^humans\.txt$', StaticView.as_view(template_name='humans.txt', content_type='text/plain; charset=utf-8')),
+urlpatterns = [
+    url(r'^robots\.txt$', StaticView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^humans\.txt$', StaticView.as_view(template_name='humans.txt', content_type='text/plain; charset=utf-8')),
 
     # A view that can be used to test exception handling
-    (r'^DivideByZeroPlease$', lambda request: 0 / 0),
+    url(r'^DivideByZeroPlease$', lambda request: 0 / 0),
 
-    url(r'^api/v1/new$', 'django_short_urls.views.new', name='new'),
-    url(r'^(.*)$', 'django_short_urls.views.main', name='main'),
-)
+    url(r'^api/v1/new$', views.new, name='new'),
+    url(r'^(.*)$', views.main, name='main'),
+]
