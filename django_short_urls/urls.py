00c231bdd187204858d60da607eba38c050c41d5
Olivier Le Floch <olivier@le-floch.fr>
2016-03-18 15:34:42 -0700

    Oops.
     __________________________________
    < Did you pay the new Support Fee? >
     ----------------------------------
            \   ^__^
             \  (oo)\_______
                (__)\       )\/\
                    ||----w |
                    ||     ||

--- django_short_urls/urls.py
+++ django_short_urls/urls.py
@@ -10,7 +10,7 @@ from django_short_urls.static_view import StaticView
 from django_short_urls import views
 
 
-urlpatterns = [
+urlpatterns = [  # pylint: disable=invalid-name
     url(r'^robots\.txt$', StaticView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^humans\.txt$', StaticView.as_view(template_name='humans.txt', content_type='text/plain; charset=utf-8')),
 
